4101de7d4ba7e233335e534288118ea345e576fa
aizawa haruka <85387680+aharu6@users.noreply.github.com>
2024-03-24 16:22:44 +0900

    ***

--- mysite/selectradioproject/admin.py
+++ mysite/selectradioproject/admin.py
@@ -9,4 +9,23 @@ class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 3
     
-class Prog
+class ProgramInline(admin.StackedInline):
+    model = Program
+    extra = 4
+    
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['question_text']}),
+        ('Date information',{'fields':['pub_date'],'classes':['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    
+admin.site.register(Question,QuestionAdmin)
+    
+class ChioceAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['choice_text']}),
+        ('Votes',{'fields':['votes'],'classes':['collapse']}),
+    ]
+    inlines = [ProgramInline]
+admin.site.register(Choice,ChioceAdmin)
